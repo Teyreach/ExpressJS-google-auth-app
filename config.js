8eee249849e4ffeee512188bf7c6749aae1cf422
Robert Koszela <dorso123@gmail.com>
2018-02-24 15:02:52 +0100

    zadanie17_7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '1018390413444-iukd81tlekrhbgcb14g2u7la8u9cqqan.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'x18tlhBX_yHz_1b9Gff0yvWC',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
